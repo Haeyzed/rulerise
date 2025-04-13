14e7c3409410b3b4e4e165ef561893c69010020b
Azeez Muibi <amuibi@softalliance.com>
2025-04-13 08:34:43 +0100

    update

--- routes/auth_api.php
+++ routes/auth_api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 // Auth routes
     // Public routes
 Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
 // Password reset
 Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
