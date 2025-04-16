af2cb39b1b0377c887fbf287f2936dd5a798b326
Azeez Muibi <amuibi@softalliance.com>
2025-04-17 00:43:35 +0100

    update

--- app/Http/Requests/Candidate/UpdateAccountSettingRequest.php
+++ app/Http/Requests/Candidate/UpdateAccountSettingRequest.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Requests\Candidate;
 
+use App\Http\Requests\BaseRequest;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
-class UpdateAccountSettingRequest extends FormRequest
+class UpdateAccountSettingRequest extends BaseRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,18 @@ class UpdateAccountSettingRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'other_name' => 'nullable|string|max:255',
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('users')->where(function ($query) {
+                    return $query->where('user_type', $this->input('user_type'));
+                }),
+            ],
+            'phone' => 'nullable|string|max:20',
+            'phone_country_code' => 'nullable|string|max:10',
         ];
     }
 }
