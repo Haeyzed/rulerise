aa9213fc28c63cc65b6067af0b1d1ee6c6478fd1
Azeez Muibi <amuibi@softalliance.com>
2025-04-27 00:41:45 +0100

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-//            PermissionSeeder::class,
-//            RoleSeeder::class,
-//            AdminSeeder::class,
-//            JobCategorySeeder::class,
+            PermissionSeeder::class,
+            RoleSeeder::class,
+            AdminSeeder::class,
+            JobCategorySeeder::class,
             SubscriptionPlanSeeder::class,
         ]);
     }
