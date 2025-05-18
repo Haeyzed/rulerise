7d16dc10405e84df05c32daa7d88d60b66e86430
Azeez Muibi <amuibi@softalliance.com>
2025-05-18 12:43:01 +0100

    Notification

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -21,7 +21,7 @@ TalentBeyondBorders
 {{-- Footer --}}
 <x-slot:footer>
 <x-mail::footer>
-© {{ date('Y') }} TalentBeyondBorders. All rights reserved.
+© {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
 </x-mail::footer>
 </x-slot:footer>
 </x-mail::layout>
