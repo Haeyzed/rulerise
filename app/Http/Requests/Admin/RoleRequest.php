29beff06d18a148b5a59a9a5f8d843abe472c6b9
Azeez Muibi <amuibi@softalliance.com>
2025-05-22 02:32:08 +0100

    Update

--- app/Http/Requests/Admin/RoleRequest.php
+++ app/Http/Requests/Admin/RoleRequest.php
@@ -23,7 +23,7 @@ class RoleRequest extends BaseRequest
      */
     public function rules(): array
     {
-        $roleId = $this->role->id; // or $this->role if you have route model binding
+        $roleId = $this->roles->id; // or $this->role if you have route model binding
 
         return [
             'name' => [
