4092a3fb242531462db5e8971eda342278dc0ae3
Azeez Muibi <amuibi@softalliance.com>
2025-06-05 23:17:20 +0100

    update

--- resources/views/emails/candidate/application-received.blade.php
+++ resources/views/emails/candidate/application-received.blade.php
@@ -6,22 +6,22 @@ Hello {{ $candidateName }},
 Your application for the position of **{{ $job->title }}** at **{{ $employer->company_name }}** has been successfully submitted.
 
 ## Application Details
-**Position:** {{ $job->title }}  
-**Company:** {{ $employer->company_name }}  
-**Location:** {{ $job->location }}  
-**Application Date:** {{ $application->created_at->format('F j, Y') }}  
+**Position:** {{ $job->title }}
+**Company:** {{ $employer->company_name }}
+**Location:** {{ $job->location }}
+**Application Date:** {{ $application->created_at->format('F j, Y') }}
 @if($resumeUsed)
-**Resume:** Your uploaded resume was included with this application  
+**Resume:** Your uploaded resume was included with this application
 @else
-**Resume:** Your profile information was used for this application  
+**Resume:** Your profile information was used for this application
 @endif
 
 ## What's Next?
 The employer will review your application and may contact you for further steps in the hiring process. You can track the status of your application in your dashboard.
 
-<x-mail::button :url="$viewApplicationUrl">
-Track Your Application
-</x-mail::button>
+{{--<x-mail::button :url="$viewApplicationUrl">--}}
+{{--Track Your Application--}}
+{{--</x-mail::button>--}}
 
 <x-mail::button :url="$viewJobUrl">
 View Job Details
