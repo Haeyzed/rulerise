18564996c01bdbed3d5da74857db828c644a2601
Azeez Muibi <amuibi@softalliance.com>
2025-06-10 15:08:28 +0100

    Update

--- database/migrations/2025_06_10_140335_add_subscription_fields_to_subscriptions_table.php
+++ database/migrations/2025_06_10_140335_add_subscription_fields_to_subscriptions_table.php
@@ -12,12 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('subscriptions', function (Blueprint $table) {
-            $table->string('payment_type')->default('recurring')->after('is_active');
-            $table->boolean('is_suspended')->default(false)->after('payment_type');
-            $table->boolean('used_trial')->default(false)->after('is_suspended');
-            $table->json('subscriber_info')->nullable()->after('used_trial');
-            $table->json('billing_info')->nullable()->after('subscriber_info');
-            $table->string('external_status')->nullable()->after('billing_info');
+//            $table->string('payment_type')->default('recurring')->after('is_active');
+//            $table->boolean('is_suspended')->default(false)->after('payment_type');
+//            $table->boolean('used_trial')->default(false)->after('is_suspended');
+//            $table->json('subscriber_info')->nullable()->after('used_trial');
+//            $table->json('billing_info')->nullable()->after('subscriber_info');
+//            $table->string('external_status')->nullable()->after('billing_info');
             $table->timestamp('status_update_time')->nullable()->after('external_status');
             $table->date('next_billing_date')->nullable()->after('status_update_time');
         });
@@ -30,12 +30,12 @@ return new class extends Migration
     {
         Schema::table('subscriptions', function (Blueprint $table) {
             $table->dropColumn([
-                'payment_type',
-                'is_suspended',
-                'used_trial',
-                'subscriber_info',
-                'billing_info',
-                'external_status',
+//                'payment_type',
+//                'is_suspended',
+//                'used_trial',
+//                'subscriber_info',
+//                'billing_info',
+//                'external_status',
                 'status_update_time',
                 'next_billing_date'
             ]);
